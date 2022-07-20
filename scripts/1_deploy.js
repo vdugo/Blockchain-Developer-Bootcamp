6701f055b72bc1c61f42b5fd1aaee0aba29c853b
Vincent <v3rdugo@yahoo.com>
2022-07-20 16:55:50 -0700

    deploy Token test contract

---
+++ scripts/1_deploy.js
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat")
+
+async function main()
+{
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
+
+  // Deploy contract
+  const token = await Token.deploy()
+  await token.deployed()
+
+  console.log(`Token deployed to ${token.address}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
