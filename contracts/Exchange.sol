7d8721f1aa1b344a7ae921e43465199ca8fa8ddf
Vincent <v3rdugo@yahoo.com>
2022-07-22 14:59:54 -0700

    set up deposits for exchange smart contract and its tests

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -3,14 +3,39 @@ pragma solidity ^0.8.0;
 
 import 'hardhat/console.sol';
 
+import './Token.sol';
+
 contract Exchange
 {
     address public feeAccount;
     uint256 public feePercent;
 
+    // keep track of how many tokens users have deposited
+    // top level is indexed by token
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(address _token, address _user, uint256 _amount, uint256 _balance);
+
     constructor(address _feeAccount, uint256 _feePercent)
     {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
+    }
+    // Deposit Tokens
+    function depositToken(address _token, uint256 _amount) public
+    {
+        // Transfers tokens to exchange
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        // Update user balance
+        tokens[_token][msg.sender] += _amount;
+        // Emit an event
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    // Check Balances
+
+    function balanceOf(address _token, address _user) public view returns(uint256)
+    {
+        return tokens[_token][_user];
     }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -16,12 +16,21 @@ describe('Exchange', () =>
 
     beforeEach(async () => 
     {
+        const Exchange = await ethers.getContractFactory('Exchange')
+        const Token = await ethers.getContractFactory('Token')
+
+        token1 = await Token.deploy('Dapp University', 'DAPP', '1000000')
+
         accounts = await ethers.getSigners()
         deployer = accounts[0]
         feeAccount = accounts[1]
+        user1 = accounts[2]
+
+        // give user1 tokens
+        let transaction = await token1.connect(deployer).transfer(user1.address, tokens(100))
+        await transaction.wait()
 
         // Fetch the token from the blockchain
-        const Exchange = await ethers.getContractFactory('Exchange')
         exchange = await Exchange.deploy(feeAccount.address, feePercent)
     })
 
@@ -35,5 +44,53 @@ describe('Exchange', () =>
         {
             expect(await exchange.feePercent()).to.equal(feePercent)
         })
-    }) 
+    })
+
+    describe('Depositing Tokens', () => 
+    { 
+        let transaction
+        let result
+        let amount
+
+        describe('Success', () => 
+        {
+            beforeEach(async () => 
+            {
+                amount = tokens(10)
+                // Approve Token
+                transaction = await token1.connect(user1).approve(exchange.address, amount)
+                // Deposit token
+                transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+                result = await transaction.wait()
+            })
+
+            it('tracks the token deposit', async () => 
+            {
+                expect(await token1.balanceOf(exchange.address)).to.equal(amount)
+                expect(await exchange.tokens(token1.address, user1.address)).to.equal(amount)
+                expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount)
+            })
+
+            it('emits a deposit event', async () => 
+            {
+                const event = result.events[1] // multiple events are emitted
+                expect(event.event).to.equal('Deposit')
+    
+                const args = event.args
+                expect(args._token).to.equal(token1.address)
+                expect(args._user).to.equal(user1.address)
+                expect(args._amount).to.equal(amount)
+                expect(args._balance).to.equal(amount)
+    
+            })
+        })
+
+        describe('Failure', () => 
+        {
+            it('fails when no tokens are approved', async () => 
+            {
+                await expect(exchange.connect(user1).depositToken(token1.address,amount)).to.be.reverted
+            })
+        })
+    })
 })
