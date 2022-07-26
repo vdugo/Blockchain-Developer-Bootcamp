82d4ca37336faa931123e0281eb24b8ced02b887
Vincent <v3rdugo@yahoo.com>
2022-07-26 13:55:25 -0700

    finish deposits

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,13 @@ import {
   loadNetwork, 
   loadAccount, 
   loadTokens,
-  loadExchange
+  loadExchange,
+  subscribeToEvents
 } from '../store/interactions'
 
 import Navbar from './Navbar'
 import Markets from './Markets'
+import Balance from './Balance'
 
 
 function App() {
@@ -37,13 +39,16 @@ function App() {
     const mETH = config[chainId].mETH
     const mDAI = config[chainId].mDAI
     // load exchange smart contract
-    const exchange = config[chainId].exchange
+    const exchangeConfig = config[chainId].exchange
 
     // Token smart contract
     await loadTokens(provider, [DApp.address, mETH.address], dispatch)
 
     // Exchange smart contract
-    await loadExchange(provider, exchange.address, dispatch)
+    const exchange = await loadExchange(provider, exchangeConfig.address, dispatch)
+
+    // Listen to events
+    subscribeToEvents(exchange, dispatch)
   }
 
   useEffect(() => 
@@ -61,7 +66,7 @@ function App() {
 
           <Markets />
 
-          {/* Balance */}
+          <Balance />
 
           {/* Order */}
 
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -44,6 +44,11 @@ export const tokens = (state = DEFAULT_TOKENS_STATE, action) =>
                 contracts: [action.token],
                 symbols: [action.symbol]
             }
+        case 'TOKEN_1_BALANCE_LOADED':
+            return {
+                ...state,
+                balances: [action.balance]
+            }
         case 'TOKEN_2_LOADED':
             return {
                 ...state,
@@ -51,12 +56,25 @@ export const tokens = (state = DEFAULT_TOKENS_STATE, action) =>
                 contracts: [...state.contracts, action.token],
                 symbols: [...state.symbols, action.symbol]
             }
+        case 'TOKEN_2_BALANCE_LOADED':
+            return {
+                ...state,
+                balances: [...state.balances, action.balance]
+            }
+
         default:
             return state
     }
 }
 
-export const exchange = (state = { loaded: false, contract: {} }, action) =>
+const DEFAULT_EXCHANGE_STATE = {
+    loaded: false, 
+    contract: {}, 
+    transaction: { isSuccessful: false}, 
+    events: []
+}
+
+export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) =>
 {
     switch(action.type)
     {
@@ -65,6 +83,52 @@ export const exchange = (state = { loaded: false, contract: {} }, action) =>
                 ...state,
                 loaded: true,
                 contract: action.exchange
+            }
+
+        // balance cases
+        case 'EXCHANGE_TOKEN_1_BALANCE_LOADED':
+            return {
+                ...state,
+                balances: [action.balance]
+            }
+        case 'EXCHANGE_TOKEN_2_BALANCE_LOADED':
+            return {
+                ...state,
+                balances: [...state.balances, action.balance]
+            }
+
+        // transfer cases
+        case 'TRANSFER_REQUEST':
+            return {
+                ...state,
+                transaction: {
+                    transactionType: 'Transfer',
+                    isPending: true,
+                    isSuccessful: false
+                },
+                transferInProgress: true
+            }
+        case 'TRANSFER SUCCESS':
+            return {
+                ...state,
+                transaction: {
+                    transactionType: 'Transfer',
+                    isPending: false,
+                    isSuccessful: true
+                },
+                transferInProgress: false,
+                events: [action.event, ...state.events]
+            }
+        case 'TRANSFER_FAIL':
+            return {
+                ...state,
+                transaction: {
+                    transactionType: 'Transfer',
+                    isPending: false,
+                    isSuccessful: false,
+                    isError: true
+                },
+                transferInProgress: false
             }
         default:
             return state
